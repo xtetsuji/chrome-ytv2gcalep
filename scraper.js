ef2d8d9709e7b27395a7821261b5137d22c4b921
MakotoNakai <macofandohr1812@gmail.com>
2021-05-04 13:02:34 +0900

    finished implementation

--- scraper.js
+++ scraper.js
@@ -12,16 +12,47 @@ axios.get(URL)
     const main = Noclass.querySelector('main');
     const article = main.querySelector('article');
     const inner = article.querySelector('.inner');
-    const narrowWrap = inner.getChild(2);
-    const sectionProgramRating = narrowWrap.querySelector('.sectionProgramRating');
+    const narrowWrap_ = inner.getChild(2);
+    const sectionProgramRating = narrowWrap_.querySelector('.sectionProgramRating');
     const programRating = sectionProgramRating.querySelector('.programRating');
     const programRatingContent = programRating.querySelector('.programRatingContent');
     const title = programRatingContent.querySelector('h1').text;
-    console.log(programRatingContent);
 
-    const channelClass = programRatingContent.querySelector('.channel');
-    // const channel = channelClass.querySelector('p').text;
-    // console.log(channel);
+    // failed
+    // const channel = programRatingContent.querySelector('.channel')
+    // const channelText = programRatingContent.querySelector('.channelText')
+    // const TVStation = channelText.text
+    // console.log(TVStation);
+
+    const programTime = inner.querySelector('.programTime');
+    const programTimeContainer = programTime.querySelector('.programTimeContainer');
+    const programTimeDetail = programTimeContainer.querySelector('.programTimeDetail');
+    const schedule = programTimeDetail.querySelector('.schedule');
+    const date_ = schedule.getChild(0);
+    const date = date_.getChild(0);
+    const month = date.text[0];
+    const day = date.text[2];
+    
+
+    const scheduleStart = schedule.getChild(0);
+    const startTime_ = scheduleStart.getChild(2);
+    const startTime = startTime_.text
+
+    const scheduleEnd = schedule.getChild(2);
+    const endTime_ = scheduleEnd.getChild(0);
+    const endTime = endTime_.text;
+
+    const narrowWrap = inner.getChild(5);
+    const programVideoContent = narrowWrap.querySelector('.programVideoContent');
+    const programVideoContentText = programVideoContent.querySelector('p');
+    const description = programVideoContentText.text;
+    
+    console.log(title);
+    console.log(month);
+    console.log(day);
+    console.log(startTime);
+    console.log(endTime);
+    console.log(description);
     
   })
   .catch(function (error) {
